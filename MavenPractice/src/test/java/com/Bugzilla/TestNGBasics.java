e036a3c81eb29419d74fa805fc53f08cb139a83e
TatTvamAsi <TatTvamAsi@Nandana.home>
2018-07-30 13:26:03 +0100

    Madhan:Pushing "MavenPractice" to github

---
+++ MavenPractice/src/test/java/com/Bugzilla/TestNGBasics.java
@@ -0,0 +1,31 @@
+package com.Bugzilla;
+
+import org.testng.annotations.Test;
+
+public class TestNGBasics {
+	
+	@Test(priority=5,enabled=true)
+	public void sum() {
+		int a=10;
+		int b=20;
+		System.out.println(a+b);
+	}
+	@Test(priority=12)
+	public void difference() {
+		int a=10;
+		int b=20;
+		System.out.println(b-a);
+	}
+	@Test(priority=23,dependsOnMethods="division")
+	public void multi() {
+		int a=10;
+		int b=20;
+		System.out.println(a*b);
+	}
+	@Test(priority=29)
+	public void division() {
+		int a=10;
+		int b=20;
+		System.out.println(b/a);
+	}
+}
