3b54a5dacf2e5ffda91122f5a4c1a371327f414f
TatTvamAsi <TatTvamAsi@Nandana.home>
2018-07-30 13:36:46 +0100

    Madhan: pushing updated code

---
+++ MavenPractice/src/test/java/com/Bugzilla/TC_LoginTest_001.java
@@ -0,0 +1,13 @@
+package com.Bugzilla;
+
+import org.testng.annotations.Test;
+
+public class TC_LoginTest_001 {
+	
+	
+	@Test
+	public void loginTestDemo() {
+		System.out.println("Testing Github");
+	}
+
+}
