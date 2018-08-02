4d0246a2a3595376d14be5c00807cd007b8053d0
TatTvamAsi <TatTvamAsi@Nandana.home>
2018-08-02 13:07:16 +0100

    commit

--- MavenPractice/src/test/java/com/Bugzilla/TC_LoginTest.java
+++ MavenPractice/src/test/java/com/Bugzilla/TC_LoginTest.java
@@ -9,13 +9,6 @@ public class TC_LoginTest{
 	public void loginTest() {
 		System.out.println("Testing Github");
 	}
-	@Test
-	public void loginTest1() {
-		System.out.println("Testing Github1");
-	}
-	@Test
-	public void loginTest2() {
-		System.out.println("Testing Github2");
-	}
+
 
 }
